c832e7a631244b55650ecee423914cfb6fab5d9f
Goran Maksimovski <goran@Gorans-MBP.lan>
2024-03-09 11:38:01 +0100

    receive-return 200

--- app/main.py
+++ app/main.py
@@ -7,9 +7,18 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    addr = "localhost"
+    port = 4221
+    server_socket = socket.create_server((addr, port), reuse_port=True)
+    connection, address = server_socket.accept()  # Accept a TCP connection
+    print(f"Connection established with {address}")
+
+    data = connection.recv(1024)  # Read data from the connection
+    print(f"Received data: {data}")
+
+    response = "HTTP/1.1 200 OK\r\n\r\n"  # Prepare the HTTP response
+    connection.sendall(response.encode())  # Send the response
+    connection.close()  # Close the connection
 
 
 if __name__ == "__main__":
