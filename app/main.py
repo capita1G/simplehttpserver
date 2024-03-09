834404f70f3e5f966819162ee55756d5b932ee16
Goran Maksimovski <goran@Gorans-MBP.lan>
2024-03-09 15:11:43 +0100

    add 200-404 response

--- app/main.py
+++ app/main.py
@@ -16,6 +16,17 @@ def main():
     response = "HTTP/1.1 200 OK\r\n\r\n"  # Prepare the HTTP response
     connection.sendall(response.encode())  # Send the response
     connection.close()  # Close the connection
+    request_line = data.decode().splitlines()[0]  # Get the request line
+    path = request_line.split()[1]  # Extract the path from the request line
+
+    resp_200 = "HTTP/1.1 200 OK\r\n\r\n"
+    resp_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
+    if path == "/":
+        response = resp_200  # Prepare the 200 OK response
+    else:
+        response = resp_404  # Prepare the 404 Not Found response
+
+    connection.sendall(response.encode())  # Send the response
 
 
 if __name__ == "__main__":
